10adc99f717f3cabbca515606ce077244f19b5c5
ticturd <148925085+ticturd@users.noreply.github.com>
2025-03-14 10:40:06 +1100

    started dice roller

--- dice-roller.py
+++ dice-roller.py
@@ -6,3 +6,31 @@
 - Class: Software Engineering
 - Description: Complete a functional dice roller app in Python
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+import random
+
+#user input function
+def get_number(prompt):
+    while True:
+        try:
+            userint = int(input(prompt))
+            return userint
+        
+        except:
+            print("Please enter a valid number")
+
+
+#amount of dice function
+def dice_amount():
+    userdiceamount = get_number("Please enter the amount of dice you want to roll:")
+    return userdiceamount
+
+#amount of dice sides function
+def dice_sides():
+    userdicesides = get_number("Please enter the amount of sides you want on your dice:")
+    return userdicesides
+
+
+
+
+
+
